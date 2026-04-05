1ccc391a68ac3cfd75ce2e71c6020aed182e9a87
bluemzzn <synthiawithsyren@gmail.com>
2026-04-06 02:27:20 +0700

    add component button

--- front-end/components/Button.tsx
+++ front-end/components/Button.tsx
@@ -1,7 +1,58 @@
-export default function ButtonCompnent(){
-    return (
-        <button className="">
-        
-        </button>
-    )
+import { cn } from "@/lib/utils";
+import { Slot } from "@radix-ui/react-slot";
+import React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+// tell HTML that our component is a button
+interface ButtonProps
+  extends
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  // variant for styling
+  asChild?: boolean;
 }
+
+const buttonVariants = cva(
+  `inline-flex items-center justify-center font-medium text-sm rounded-md`,
+
+  {
+    variants: {
+      variant: {
+        primary: "bg-[#2D5494] hover:bg-[#254579] text-white",
+        secondary: "",
+        destructive: "",
+        ok: "",
+        link: "",
+        outline : 'bg-transparent hover:bg-gray-100 text-black border border-black'
+      },
+      size: {
+        default: "px-8 py-2",
+        sm: "px-4 py-2",
+        lg: "px-14 py-4",
+        xl: "px-16 py-4",
+      },
+      defaultVariants: {
+        variant: "primary",
+        size: "default",
+      },
+    },
+  },
+);
+
+const CustomButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button";
+
+    return (
+      <Comp
+        ref={ref}
+        className={cn(buttonVariants({ variant, size, className }))}
+        {...props}
+      />
+    );
+  },
+);
+
+CustomButton.displayName = "Button";
+
+export { CustomButton, buttonVariants };
---
+++ front-end/lib/utils.ts
@@ -0,0 +1,8 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs : ClassValue[]){
+    return twMerge(clsx(inputs))
+}
+
+// cn functions is for combine diff class names since we will be customizing own components
