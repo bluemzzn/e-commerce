3954a4a0200e6c04e945296e9df8181cab5db24b
bluemzzn <synthiawithsyren@gmail.com>
2026-04-06 02:29:33 +0700

    sign in page simple ui

--- front-end/app/(registration)/signin/page.tsx
+++ front-end/app/(registration)/signin/page.tsx
@@ -1,16 +1,17 @@
+import { CustomButton } from "@/components/Button";
 import Image from "next/image";
 
 export default function SignIn() {
   return (
-    <>
+    <div className="flex flex-col mx-auto justify-center items-center">
       <div className="signup-navbar ">
         <div></div>
-        <h1>Sign Up</h1>
+        <h1 className="font-md text-4xl">Sign Up</h1>
       </div>
       <div className="wrapper border w-fit p-5">
-        <h1>Sign Up</h1>
+        <h1 className="my-2 font-semibold">Sign Up</h1>
         <input
-          className="border px-2"
+          className="border px-2 w-full"
           type="text"
           name="username"
           id="username"
@@ -19,7 +20,7 @@ export default function SignIn() {
         <br />
 
         <input
-          className="border my-2 px-2"
+          className="border my-2 px-2 w-full"
           type="email"
           name="email"
           id="email"
@@ -28,23 +29,28 @@ export default function SignIn() {
 
         <br />
         <input
-          className="border px-2"
+          className="border px-2 w-full"
           type="text"
           name="password"
           id="password"
           placeholder="password"
         />
         <br />
-        <button>Sign Up</button>
+        <CustomButton className="w-full my-4" variant={"primary"} size={"sm"}>Sign Up</CustomButton>
 
-        <div className="flex justify-center items-center gap-2 w-full">
+        <div className="flex gap-2">
+        <p>Already have an account?</p>
+        <a href="" className="underline">Log in</a>
+        </div>
+
+        <div className="flex justify-center items-center gap-2 w-full mb-4">
           <hr className="w-1/3" />
           <p>or</p>
           <hr className="w-1/3" />
           <br />
         </div>
 
-        <div className="google-login-button flex gap-2 border w-fit px-4 py-3 rounded-sm">
+        <CustomButton className="google-login-button flex gap-2 w-full px-4 py-3" variant={"outline"}>
           <Image
             className=""
             src="/google.svg"
@@ -53,8 +59,8 @@ export default function SignIn() {
             height={20}
           />
           <p>Sign up with Google</p>
-        </div>
+        </CustomButton>
       </div>
-    </>
+    </div>
   );
 }
